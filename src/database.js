c2f2802cb1ae1e13bc232881536ae43878a7464c
nacho12091 <ignaciolima12091@gmail.com>
2022-01-13 22:27:08 -0300

    Innitial commit

---
+++ src/database.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const URI = process.env.MONGODB_URI || "mongodb://localhost/testdatabase";
+
+mongoose.connect(URI);
+
+const connection = mongoose.connection;
+
+connection.once("open", () => {
+    console.log("Connected DB");
+})
---
+++ src/routes/users.js
@@ -0,0 +1,19 @@
+const {
+    Router
+} = require("express");
+const {
+    getUsers,
+    createUser,
+    deleteUser
+} = require("../controllers/users.controller");
+
+const router = Router();
+
+router.route("/")
+    .get(getUsers)
+    .post(createUser);
+
+router.route("/:id")
+    .delete(deleteUser);
+
+module.exports = router;
